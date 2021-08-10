60d70f4e12989962fa2a880451f8f2b56762d1ac
Ronan Adriel Zenatti <ronan.zenatti@etec.sp.gov.br>
2021-08-09 22:24:52 -0300

    Filtro de Pokemons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -26,9 +26,46 @@ export class HomePage {
       tipos: [
         'Fire'
       ]
+    },
+    {
+      numero: '007',
+      nome: 'Squirtle',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png',
+      tipos: [
+        'Water'
+      ]
+    },
+    {
+      numero: '025',
+      nome: 'Pikachu',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png',
+      tipos: [
+        'Electric'
+      ]
     }
   ];
 
-  constructor() { }
+  listaPokemonFiltrada = [];
+
+  constructor() {
+    this.retonarPokemon();
+  }
+
+  retonarPokemon(): void {
+    this.listaPokemonFiltrada = this.listaPokemon;
+  }
+
+  buscarPokemon(evento): void {
+    this.retonarPokemon(); // Coloca todos os pokemons na lista Filtrada
+
+    // Pega o valor digitado no campo de busca.
+    const busca: string = evento.target.value;
+
+    if (busca && busca.trim() !== '') { // Testa se tem alguma coisa no campo
+      this.listaPokemonFiltrada = this.listaPokemon.filter(pokemon =>
+        pokemon.nome.toLowerCase().includes(busca.trim().toLowerCase())
+      );
+    }
+  }
 
 }
--- src/app/pokemons/bulbasaur/bulbasaur.page.ts
+++ src/app/pokemons/bulbasaur/bulbasaur.page.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BulbasaurPage implements OnInit {
 
+  pokemon = {
+
+  };
+
   constructor() { }
 
   ngOnInit() {
